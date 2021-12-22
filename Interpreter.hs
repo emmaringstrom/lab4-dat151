541f684669c97542be306090f756ec26aa019623
viklofg <63787568+viklofg@users.noreply.github.com>
2021-12-22 14:47:27 +0100

    klart

--- Interpreter.hs
+++ Interpreter.hs
@@ -15,17 +15,13 @@ import Data.Functor
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-import Debug.Trace
-
-
 import Fun.Abs
 import Fun.Print
 import Control.Monad.State
 import qualified Data.List as List
 import Prelude hiding (lookup)
 
---data Value = N Integer | C Closure
-newtype Closure = Closure (Exp, Env)
+data Closure = Closure (Exp, Env)
   deriving Show
 
 data Env = Env {
@@ -46,63 +42,76 @@ data Strategy
 type Err = Except String
 
 eval :: Env -> Exp -> Strategy -> Err Closure
---eval env exp s | trace (show exp ++ "\n" ++ show (vars env) ++ "\n\n") False = undefined
+-- eval env exp s | trace (show exp ++ "\n" ++ show (vars env) ++ "\n\n") False = undefined
 eval env exp s = case exp of
+  
   EVar id -> do
-    let v@(Closure (exp', gamma)) = lookup id env
-    let env' = Env{vars = vars gamma, funs = funs env}
-    eval env' exp' s
+    let Closure (exp', gamma) = lookup id env
+    eval Env{vars = vars gamma, funs = funs env} exp' s
+  
   EInt i -> return $ Closure (EInt i, emptyEnv)
+  
   EApp id e -> do
-    v <- eval env id s
-    let Closure (b, delta) = v
-    let env' = Env{vars = vars delta, funs = funs env} 
-    let x = head $ Map.keys (vars delta)
-    if s == CallByValue then do
-      u <- eval env e s
-      let env'' = updateVar env' x u
-      eval env'' b s 
-    else do
-      let u = Closure (e, Env{vars = vars env, funs = Map.empty})
-      let env'' = updateVar env' x u 
-      eval env'' b s 
-    -- (\x.b){delta} = eval(gamma id)
-    --eval(update((functions(gamma), delta), x, u), b)
-    --env' <- updateFun
+    closure <- eval env id s
+    case closure of 
+      Closure (EAbs id' exp', delta) -> do
+        if s == CallByValue then do
+          u <- eval env e s
+          eval (updateVar Env{vars = vars delta, funs = funs env} id' u) exp' s
+        else do
+          let u = Closure (e, Env{vars = vars env, funs = Map.empty})
+          eval (updateVar Env{vars = vars delta, funs = funs env} id' u) exp' s
+      _ -> error "Invalid function application"
+
   EAdd e1 e2 -> do
     c1 <- eval env e1 s 
-    let Closure (EInt e1', _) = c1
+    let Closure (e1', _) = c1
     c2 <- eval env e2 s 
-    let Closure (EInt e2', _) = c2
-    return $ Closure (EInt (e1' + e2'), emptyEnv)
+    let Closure (e2', _) = c2
+    case (e1', e2') of
+      (EInt i, EInt j) -> return $ Closure (EInt (i+j), emptyEnv)
+      _ -> error "Addition is only defined for integers"
+    
   ESub e1 e2 -> do
     c1 <- eval env e1 s 
-    let Closure (EInt e1', _) = c1
+    let Closure (e1', _) = c1
     c2 <- eval env e2 s
-    let Closure (EInt e2', _) = c2
-    return $ Closure (EInt (e1' - e2'), emptyEnv)
+    let Closure (e2', _) = c2
+    case (e1', e2') of
+      (EInt i, EInt j) -> return $ Closure (EInt (i-j), emptyEnv)
+      _ -> error "Subtraction is only defined for integers"
+  
   ELt e1 e2 -> do
-    Closure (e1', _) <- eval env e1 s
-    Closure (e2', _) <- eval env e2 s
-    if e1' < e2' then return $ Closure (EInt 1, emptyEnv)  else return $ Closure (EInt 0, emptyEnv)
+    e1'' <- eval env e1 s
+    let Closure (e1', _) = e1''
+    e2'' <- eval env e2 s
+    let Closure (e2', _) = e2''
+    case (e1', e2') of
+      (EInt i, EInt j) | i < j -> return $ Closure (EInt 1, emptyEnv)
+      (EInt i, EInt j) -> return $ Closure (EInt 0, emptyEnv)
+      _ -> error "Less than operator is only defined for integers"
+  
   EIf e1 e2 e3 -> do
-    Closure (e1', _) <- eval env e1 s 
-    if e1' == EInt 1 then eval env e2 s
-      else eval env e3 s
-  EAbs id e -> return $ Closure (exp, Env{vars = vars env, funs = Map.empty})-- eval  e s
+    e1'' <- eval env e1 s
+    let Closure (e1', _) = e1''
+    case e1' of 
+      EInt 0 -> eval env e3 s
+      EInt 1 -> eval env e2 s
+      _ -> error "If/then/else statement is only defined for booleans"
+  
+  EAbs id e -> return $ Closure (exp, Env{vars = vars env, funs = Map.empty})
 
 lookup :: Ident -> Env -> Closure
-lookup id env = case Map.lookup id (vars env) of
+lookup id@(Ident name) env = case Map.lookup id (vars env) of
     Just closure -> closure
     Nothing -> case Map.lookup id (funs env) of
-      Nothing -> error $ "lookup " ++ show id
+      Nothing -> error $ "Unknown variable: " ++ name
       Just closure -> closure
 
 updateFun :: Env -> Ident -> Closure -> Env
 updateFun env id val = Env{vars = vars env, funs = Map.insert id val (funs env)}
 
 updateVar :: Env -> Ident -> Closure -> Env
-updateVar env id cl | trace (show id ++ "\n") False = undefined
 updateVar env id val = Env{vars = Map.insert id val (vars env), funs = funs env}
 
 emptyEnv :: Env
@@ -111,14 +120,16 @@ emptyEnv = Env{vars = Map.empty, funs = Map.empty}
 updateDefs :: [Def] -> Env -> Env
 updateDefs [] env = env
 updateDefs (def@(DDef id ids exp):defs) env = do
-  let closure = Closure (makeLambda def, emptyEnv)
+  let closure = makeClosure def
   let env' = updateFun env id closure
   updateDefs defs env'
 
-makeLambda :: Def -> Exp
-makeLambda (DDef name [] exp) = (EAbs (Ident "dummyID") exp)
-makeLambda (DDef name (id:ids) exp) = 
-  (EAbs id (makeLambda (DDef name ids exp)))
+makeClosure :: Def -> Closure
+makeClosure (DDef name [] exp) = Closure (exp, emptyEnv)
+makeClosure (DDef name (id:ids) exp) = do
+  let Closure (exp', env') = makeClosure (DDef name ids exp)
+  let exp = EAbs id exp'
+  Closure (exp, env')
 
 -- | Entry point: Program computes a number.
 
@@ -126,6 +137,7 @@ interpret :: Strategy -> Program -> Err Integer
 interpret strategy (Prog defs (DMain mainExp)) = do
   let env = updateDefs defs emptyEnv
   closure <- eval env mainExp strategy
-  error $ "closure " ++ show closure
-  let Closure (EInt i, _) = closure
-  return i
+  case closure of
+    Closure (EInt i, _) -> return i
+    _ -> error "Main must evaluate to integer"
+
